020c2485409a9c2b484c6fc6587589a5b57a3daa
wcj <wcj@163.com>
2021-08-05 15:21:31 +0800

    inputView

---
+++ WCJFM/WCJInputView/WCJInputView.h
@@ -0,0 +1,16 @@
+//
+//  WCJInputView.h
+//  WCJFM
+//
+//  Created by wen chuanjun on 2021/8/5.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WCJInputView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WCJFM/WCJInputView/WCJInputView.m
@@ -0,0 +1,20 @@
+//
+//  WCJInputView.m
+//  WCJFM
+//
+//  Created by wen chuanjun on 2021/8/5.
+//
+
+#import "WCJInputView.h"
+
+@implementation WCJInputView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
